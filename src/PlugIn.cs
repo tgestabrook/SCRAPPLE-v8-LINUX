9cacc49a65f6d5be84ddb120c21b92e3927d60b0
Thomas <thomas.estabrook@usda.gov>
2024-12-10 14:14:14 -0800

    Added CWD and PET output maps

--- src/PlugIn.cs
+++ src/PlugIn.cs
@@ -680,7 +680,59 @@ namespace Landis.Extension.SocialClimateFire
                     outputRaster.WriteBufferPixel();
                 }
             }
+
+            string[] paths10 = { "social-climate-fire", "PET-{timestep}.tif" };
+            path = MapNames.ReplaceTemplateVars(Path.Combine(paths10), currentTime);
+            using (IOutputRaster<IntPixel> outputRaster = modelCore.CreateRaster<IntPixel>(path, modelCore.Landscape.Dimensions))
+            {
+                IntPixel pixel = outputRaster.BufferPixel;
+                foreach (Site site in PlugIn.ModelCore.Landscape.AllSites)
+                {
+                    if (site.IsActive)
+                    {
+                        if (SiteVars.Disturbed[site] && SiteVars.Intensity[site] > 0)
+                        {
+                            pixel.MapCode.Value = (int)(SiteVars.PotentialEvapotranspiration[site]);
+                        }
+                        else
+                            pixel.MapCode.Value = 0;
+                    }
+                    else
+                    {
+                        //  Inactive site
+                        pixel.MapCode.Value = 0;
+                    }
+                    outputRaster.WriteBufferPixel();
+                }
+            }
+
+            string[] paths10 = { "social-climate-fire", "CWD-{timestep}.tif" };
+            path = MapNames.ReplaceTemplateVars(Path.Combine(paths10), currentTime);
+            using (IOutputRaster<IntPixel> outputRaster = modelCore.CreateRaster<IntPixel>(path, modelCore.Landscape.Dimensions))
+            {
+                IntPixel pixel = outputRaster.BufferPixel;
+                foreach (Site site in PlugIn.ModelCore.Landscape.AllSites)
+                {
+                    if (site.IsActive)
+                    {
+                        if (SiteVars.Disturbed[site] && SiteVars.Intensity[site] > 0)
+                        {
+                            pixel.MapCode.Value = (int)(SiteVars.ClimaticWaterDeficit[site]);
+                        }
+                        else
+                            pixel.MapCode.Value = 0;
+                    }
+                    else
+                    {
+                        //  Inactive site
+                        pixel.MapCode.Value = 0;
+                    }
+                    outputRaster.WriteBufferPixel();
+                }
+            }
         }
+
+
 
         //---------------------------------------------------------------------
 
